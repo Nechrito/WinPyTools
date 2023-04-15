46083ce60226a6fd03dddbbf88f2abb837af382f
Philip Lindh <philip.lindh98@hotmail.com>
2023-04-15 19:20:45 +0200

    Detailed visualization and a simple WIP version

--- src/unity_code_analyzer.py
+++ src/unity_code_analyzer.py
@@ -32,8 +32,10 @@ classDiagram
     Customer "1" --> "*" Ticket
     Student "1" --> "1..*" Course
     Galaxy --> "many" Star : Contains
-
 """
+
+# https://mermaid.js.org/syntax/classDiagram.html
+
 import os
 import shutil
 import sys
@@ -43,6 +45,7 @@ from collections import defaultdict
 
 def find_most_common_relationship(relationships):
     relationship_counts = defaultdict(int)
+    
     for relationship in relationships:
         relationship_counts[relationship[1]] += 1
 
@@ -132,12 +135,63 @@ def generate_mermaid_script(relationships, class_info):
     return mermaid_script.replace('"', '')
 """
 
-def get_class_members(class_name, class_info):
-    members = class_info[class_name].get('members', [])
-    members_str = "\n".join([f"    {member}" for member in members])
-    return members_str
+def class_info_to_plantuml(class_info):
+    #plantuml_code = "@startuml\n"
+    plantuml_code = "classDiagram\n"
+
+    for class_name, info in class_info.items():
+        base_class_name = info.get('base', '')
+        members = info.get('members', [])
+        methods = info.get('methods', [])
+
+        if base_class_name:
+            plantuml_code += f"{class_name} --|> {base_class_name}\n"
+
+        #plantuml_code += f"class {class_name} {{\n"
+        plantuml_code += f'class {class_name} {{\n'
+        plantuml_code += f"    \n"
         
-import re
+
+        for member in members:
+            access, _, member_type, member_name = member
+            plantuml_code += f"  {access.strip()} {member_type} {member_name}\n"
+
+        for method in methods:
+            access, _, return_type, method_name = method
+            plantuml_code += f"  {access.strip()} {return_type} {method_name}()\n"
+
+        plantuml_code += "}\n"
+
+    #plantuml_code += "@enduml\n"
+
+    return plantuml_code
+
+def unpack_to_mermaid_code(class_info):
+    mermaid_code = "classDiagram\n"
+
+    for class_name, info in class_info.items():
+        base_class_name = info.get('base', '')
+        members = info.get('members', [])
+        methods = info.get('methods', [])
+
+        if base_class_name:
+            mermaid_code += f"{class_name} <|-- {base_class_name}\n"
+
+        mermaid_code += f"class {class_name} {{\n"
+
+        for member in members:
+            access, _, member_type, member_name = member
+            mermaid_code += f"  {access.strip()} {member_type} {member_name}\n"
+
+        for method in methods:
+            access, _, return_type, method_name = method
+            mermaid_code += f"  {access.strip()} {return_type} {method_name}()\n"
+
+        mermaid_code += "}\n"
+
+    return mermaid_code
+
+
 def parse_csharp_code(file_path):
     relationships = []
 
@@ -163,115 +217,51 @@ def parse_csharp_code(file_path):
     for match in class_matches:
         class_name = match[0]
         base_class_name = match[1]
-        members_and_methods = re.findall(r'([+\-#])(\w+\s+\w+(?:\(.*?\))?)', match[2])
 
-        for member in members_and_methods:
-            member = [m.strip() for m in member]
-            member = [re.sub(r'\s+', ' ', m) for m in member]
-            member = [re.sub(r'(public|private|protected)\s+', '' , m) for m in member]
-            member = [re.sub(r'\s+\w+\s+\w+\(.*?\)', '' , m) for m in member]
-            member = [re.sub(r'\(.*?\)', '', m) for m in member]
-            member = [re.sub(r';', '', m) for m in member]
-            member = [re.sub(r'{\s*get;\s*set;\s*}', '', m) for m in member]
-            member = [re.sub(r'{\s*get;\s*}', '', m) for m in member]
-            member = [re.sub(r'{\s*set;\s*}', '', m) for m in member]
-            member = [re.sub(r'{\s*}', '', m) for m in member]
-            member = [re.sub(r'{\s*get;\s*set;\s*} =', '', m) for m in member]
-            member = [re.sub(r'{\s*get;\s*} =', '', m) for m in member]
-            member = [re.sub(r'{\s*set;\s*} =', '', m) for m in member]
-            member = [re.sub(r'{\s*} =', '', m) for m in member]
-            member = [re.sub(r'{\s*get;\s*set;\s*} =>', '', m) for m in member]
-            member = [re.sub(r'{\s*get;\s*} =>', '', m) for m in member]
-            member = [re.sub(r'{\s*}', '', m) for m in member]
-            member = [re.sub(r'\bnew\b', '', m) for m in member]
-            member = [re.sub(r'\bstatic\b', '', m) for m in member]
-            member = [re.sub(r'\boverride\b', '', m) for m in member]
-            member = [re.sub(r'\babstract\b', '', m) for m in member]
-            member = [re.sub(r'\bvirtual\b', '', m) for m in member]
-            member = [re.sub(r'\bsealed\b', '', m) for m in member]
-            member = [re.sub(r'\breadonly\b', '', m) for m in member]
-            member = [re.sub(r'\bconst\b', '', m) for m in member]
-            member = [re.sub(r'\s+', ' ', m) for m in member]
-            member = [re.sub(r'(public|private|protected)\s+', '' , m) for m in member]
-            
-            #class_members[member[0]] = member[1]
-        #class_info[class_name] = class_members
+        class_data = match[2]
+        members = re.findall(r'((public|private|protected)\s+)?(\w+)\s+(\w+);', class_data)
+        methods = re.findall(r'((public|private|protected)\s+)?(\w+)\s+(\w+)\s*\(', class_data)
 
-        class_info[class_name]['members_and_methods'] = members_and_methods
+        class_info[class_name]['members'] = members
+        class_info[class_name]['methods'] = methods
 
     return relationships, class_info
 
-def generate_mermaid_script(relationships, class_info):
-    mermaid_script = '''classDiagram\n'''
 
-    for relationship in relationships:
-        #class_name1 = format_class_name(relationship[0])
-        
-        class_name1 = relationship[0]
-        class_name2 = relationship[1]
-        
-        mermaid_script += f'{class_name1} <|-- {class_name2}\n'
+def parse_relationships_from_class_file(class_file):
+    relationships = []
+    
+    try:
+        with open(class_file, 'r') as f:
+            for line in f:
+                if 'relationship' in line:
+                    relationship = line.replace('relationship', '').strip().split()
+                    relationships.append(relationship)
+    except FileNotFoundError:
+        print(f'Unable to find file: {class_file}')
 
-    for class_name, members_and_methods in class_info.items():
-        
-        if len(members_and_methods) > 0:
-            
-            #print(f'Class: {class_name}------------------\nMembers: {members_and_methods}\n------------------')
-        
-            mermaid_script += f'class {class_name} {{\n'
-            
-            #mermaid_script += '    +constructor()\n'
-            #mermaid_script += '    \n'
-            #mermaid_script += f'    {members_and_methods}\n'
-            
-            #for member in members_and_methods:
-            mermaid_script += f'    {class_info["members_and_methods"]}\n'
-            
-            mermaid_script += '}\n'
-        
-
-    return mermaid_script
+    return relationships
 
 def write_mermaid_file(relationships, output_file, class_info):
-    
-    # for relationship in relationships:
-    #     class_name1 = format_class_name(relationship[0])
-    #     class_name2 = format_class_name(relationship[1])
-    
-    # TODO: Append suffix number to output_file if it exists already 
-    # TODO: If output_file exists, try to parse it and append new relationships to it
+    mermaid_script = []
+    mermaid_script.append("classDiagram")
 
-    # TODO: If output_file exists, delete it and generate a new one
-    if os.path.exists(output_file):
-        
-        # Get the suffix number from files with the same name
-        output_file = output_file.replace('.txt', '')
-        output_file = output_file.replace('.md', '')
-        output_file = output_file.replace('.mmd', '')
-        
-        suffix = 1
-        
-        while os.path.exists(f'{output_file}{suffix}.txt') or os.path.exists(f'{output_file}{suffix}.md') or os.path.exists(f'{output_file}{suffix}.mmd'):
-            suffix += 1
-        
-        output_file = f'{output_file}{suffix}.mmd'
-        
-        print(f'Output file {output_file} already exists. A new file will be created with the suffix {suffix}.')
-        
-        #os.remove(output_file)
-        
-        #with open(output_file, 'r', encoding='utf-8') as file:
-            #mermaid_script = file.read()
-    #else:
-        #mermaid_script = ''
-    
-    mermaid_script = generate_mermaid_script(relationships, class_info)
+    for rel in relationships:
+        base_class = rel[0]
+        derived_class = rel[1]
 
-    with open(output_file, "w", encoding='utf-8') as f:
+        base_class = base_class.replace(".", "_")
+        derived_class = derived_class.replace(".", "_")
+
+        mermaid_script.append(f"{derived_class} --|> {base_class}")
+
+    mermaid_script = "\n".join(mermaid_script)
+
+    with open(output_file, "w") as f:
         f.write(mermaid_script)
 
-    print(f"Mermaid script has been generated as {output_file}")
 
+        
 def main():
     if len(sys.argv) < 2:
         print("Usage: python unity_code_analyzer.py <directory_with_csharp_files>")
@@ -280,6 +270,7 @@ def main():
     directory = sys.argv[1]
 
     all_relationships = []
+    
     class_info = defaultdict(dict)
 
     for root, dirs, files in os.walk(directory):
@@ -291,15 +282,23 @@ def main():
                 class_info.update(file_class_info)
 
     output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "mermaid")
-    
+
     if os.path.exists(output_dir):
         shutil.rmtree(output_dir)
         # os.remove(output_dir)
-    
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    relations_per_file = 20
+    relations_per_file = 15
+    
+    # convert class_info to plantuml
+    plantuml_code = class_info_to_plantuml(class_info)
+    
+    print(plantuml_code)
+    
+    with open('plantuml_code.puml', 'w') as f:
+        f.write(plantuml_code)
 
     for i in range(0, len(all_relationships), relations_per_file):
         relationships_chunk = all_relationships[i:i+relations_per_file]
