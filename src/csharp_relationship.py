be10286888ee398792579a185aad9f874fe4f221
Philip Lindh <philip.lindh98@hotmail.com>
2023-04-17 01:45:19 +0200

    x

--- src/csharp_relationship.py
+++ src/csharp_relationship.py
@@ -3,13 +3,20 @@ import re
 import subprocess
 from class_info import ClassInfo, get_package_name, get_class_name
 from plantuml import PlantUML
-project_path = "C:\Apps\The-Prophecy-of-Hank\HandyHank\Assets\HandyHank\Scripts" #input("Enter the project path: ")
+project_path = "C:\Apps\The-Prophecy-of-Hank\HandyHank\Assets\HandyHank\Scripts\Runtime" #input("Enter the project path: ")
 
 def unity_format(code):
-    return code\
-.replace(" *-- MonoBehaviour", "").replace(" *-- SerializedMonoBehaviour", "")\
-.replace(" *-- ScriptableObject", "").replace(" *-- SerializedScriptableObject", "")\
-.replace(" *-- IDisposable", "").replace(" *-- SerializedMonoBehaviour", "")\
+    res = code\
+.replace(" --> MonoBehaviour", "").replace(" --> SerializedMonoBehaviour", "")\
+.replace(" --> ScriptableObject", "").replace(" --> SerializedScriptableObject", "")\
+.replace(" --> IDisposable", "")\
+.replace(" --> Singleton", "")
+    
+    # # remove lines where there is only one word (e.g. "Hank")
+    res = re.sub(r'(?m)^\s*\w+\s*$', '', res)
+    # trim lines
+    res = re.sub(r'(?m)^\s+', '', res)
+    return res
 
 def generate_plantuml_image(plantuml_code, output_path):
     plantuml_instance = PlantUML(url='http://www.plantuml.com/plantuml/img/')
@@ -85,7 +92,7 @@ def get_class_name(cs_file):
     return os.path.splitext(os.path.basename(cs_file))[0]
 
 def generate_mermaid_diagram_chunks(cs_files, project_path):
-    class_diagram = ["flowchart LR"]
+    class_diagram = ["flowchart RL"]
     packages = defaultdict(list)
     class_info_dict = {}
 
@@ -95,28 +102,31 @@ def generate_mermaid_diagram_chunks(cs_files, project_path):
         package_name = get_package_name(cs_file, project_path)
         packages[package_name].append(class_name)
     
+    # Generate class nodes
+    # Apply: a --> b --> c
+    for cs_file in cs_files:
+        class_name = get_class_name(cs_file)
+        for inheritance in class_info_dict[cs_file].class_inheritance:
+            class_diagram.append(f"{class_name} --> {inheritance}")
+    
+    # for cs_file in cs_files:
+    #     class_name = get_class_name(cs_file)
+    #     #[class_diagram.append(f"{class_name} -.-> {relationship}") for relationship in class_info_dict[cs_file].using_statements if "Runtime" in relationship]
+    #     for relationship in class_info_dict[cs_file].using_statements:
+    #        if "Runtime" in relationship:
+    #            class_diagram.append(f"{class_name} -.-> {relationship}")
+               
+    class_diagram = [unity_format(line) for line in class_diagram]
+    
     #Generate package nodes
     #Apply: a --> b --> c
     for package_name, classes in packages.items():
+        if len(classes) <= 1:
+            continue
         class_diagram.append(f"subgraph {package_name}")
         for class_name in classes:
             class_diagram.append(f"  {class_name}")
         class_diagram.append("end")
-
-    # Generate class nodes
-    # Apply: a --> b --> c
-    # for cs_file in cs_files:
-    #     class_name = get_class_name(cs_file)
-    #     for inheritance in class_info_dict[cs_file].class_inheritance:
-    #         class_diagram.append(f"{class_name} --> {inheritance}")
-
-    # Generate class relationships
-    # Apply: a -.-> b
-    #for cs_file in cs_files:
-    #    class_name = get_class_name(cs_file)
-    #    for relationship in class_info_dict[cs_file].relationships:
-    #        class_diagram.append(f"{class_name} -.-> {relationship}")
-
     
     return "\n".join(class_diagram)
 
@@ -168,7 +178,8 @@ def print_relationships(cs_files):
 
 cs_files = find_cs_files(project_path)
 
-mermaid_diagram = unity_format(generate_mermaid_diagram_chunks(cs_files, project_path))
+mermaid_diagram = generate_mermaid_diagram_chunks(cs_files, project_path)
+mermaid_diagram = re.sub(r'(?m)^\s+', '', mermaid_diagram)
 
 import pyperclip
 
