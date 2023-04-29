a5f0ea18b2d8d38a69ae50f1d9fb001d00139e15
Philip Lindh <philip.lindh98@hotmail.com>
2023-04-29 20:23:41 +0200

    works ish

--- src/file_tree.py
+++ src/file_tree.py
@@ -4,6 +4,7 @@ import sys
 from typing import List
 
 import print_to_json
+
 class FilePrinter:
     def __init__(self, hide_directory_name=False, hide_file_name=False):
         self.hide_directory_name = hide_directory_name
@@ -22,63 +23,67 @@ class FilePrinter:
         text = prefix + "├── 📁 " + entry
         print(text)
         return text
+
+    def print_linebreak(self):
+        text = "\n-----\n\n"
+        print(text)
+        return text
+
 class FolderStructureProcessor:
     def __init__(self, depth_limit: int=None, line_limit: int=None, word_limit: int=3159):
         self.printer = FilePrinter()
-        
+
         self.depth_limit = depth_limit
         self.line_limit = line_limit
-        self.word_limit = word_limit
-        
-        self.total_depth_count = 0
-        self.total_line_count = 0
-        self.total_word_count = 0
-        
+        self.word_limit = word_limit * 2
+
+        self.total_depth = 0
+        self.total_line_count = 1
+        self.total_words = 0
+
     def summarize(self):
-        print(f"\n\n ----- SUMMARY ----- \n")
+        print(f"\n{'='*20}\nSUMMARY\n{'='*20}\n")
         
-        print("Total depth count: " + str(self.total_depth_count))
-        print("Total line count: " + str(self.total_line_count))
-        print("Total word count: " + str(self.total_word_count))
-
-    def get_structure(self, directory, prefix="", blacklisted_dirs=None, whitelisted_filetypes: List[str] = None, depth: int=0, lines: int=0, words: int=0):
-        printed_lines = 0
-        word_count = 0
-
-        # Use 'self' to access instance variables
-        self.total_depth_count += depth
-        self.total_line_count += lines
-        self.total_word_count += words
-
-        if self.word_limit and self.total_word_count >= self.word_limit:
-            return printed_lines, word_count
+        self.depth_limit = self.depth_limit or '\u221e'
+        self.line_limit = self.line_limit or '\u221e'
+        self.word_limit = self.word_limit or '\u221e'
+        
+        print("Max depth: " + str(self.total_depth - 1) + "/" + str(self.depth_limit))
+        print("Total line count: " + str(self.total_line_count) + "/" + str(self.line_limit))
+        print("Total word count: " + str(self.total_words // 2) + "/" + str(self.word_limit // 2))
+    def get_structure(self, directory, prefix="", blacklisted_dirs=None, whitelisted_filetypes: List[str] = None, depth: int=0, lines: int=0):
+        
+        self.total_depth = max(self.total_depth, depth)
+        
+        if (self.depth_limit and depth > self.depth_limit) or (self.word_limit and self.total_words >= self.word_limit):
+            self.printer.print_linebreak()
+            lines = 0
+            return lines
 
         with os.scandir(directory) as entries:
             sorted_entries = sorted(entries, key=lambda e: e.name.lower())
 
             for entry in sorted_entries:
+                
                 # Skip blacklisted directories and hidden files
-                if blacklisted_dirs and entry.name in blacklisted_dirs or entry.name.startswith("."):
+                if (blacklisted_dirs and entry.name in blacklisted_dirs) or entry.name.startswith("."):
                     continue
-
+                
                 if entry.is_dir():
-                    if self.depth_limit and depth > self.depth_limit:
-                        return printed_lines, word_count
 
-                    if self.line_limit and printed_lines >= self.line_limit:
-                        print("\n-----\n\n")
-                        printed_lines = 0
+                    if self.line_limit and lines >= self.line_limit:
+                        self.printer.print_linebreak()
+                        lines = 0
 
                     text = self.printer.print_directory(prefix, entry.name)
+                    words = len(text.split(' '))
+                    self.total_words += words
+                    self.total_line_count += 1
 
-                    printed_lines += 1
+                    self.get_structure(entry.path, prefix + "│   ", blacklisted_dirs, whitelisted_filetypes, depth + 1, lines)
 
-                    word_count = len(text.split(' '))
-
-                    printed_lines_sub, word_count_sub = self.get_structure(entry.path, prefix + "│   ", blacklisted_dirs, whitelisted_filetypes, depth + 1)
-
-                    word_count += word_count_sub
-                    printed_lines += printed_lines_sub
+                    if self.word_limit and self.total_words >= self.word_limit:
+                        break
 
                 else:
                     # Skip files that are not of whitelisted filetypes
@@ -95,13 +100,14 @@ class FolderStructureProcessor:
                             continue
 
                     text = self.printer.print_file(prefix, entry.name)
-
-                    printed_lines += 1
-
-                    word_count = len(text.split(' '))
-
-
-        return printed_lines, word_count
+                    words = len(text.split(' '))
+                    self.total_words += words
+                    self.total_line_count += 1
+                    
+                    if self.word_limit and self.total_words >= self.word_limit:
+                        break
+                    
+        return lines
 
 if __name__ == "__main__":
     
@@ -113,7 +119,15 @@ if __name__ == "__main__":
 
     directory_to_scan = os.getcwd() + "../../" + directory_to_process
 
-    processor = FolderStructureProcessor(depth_limit=3, line_limit=20, word_limit=1000)
-    printed_lines, word_count = processor.get_structure(directory_to_scan, prefix='│   ', whitelisted_filetypes=[".cs"], depth=0, lines=0, words=0)
+    # GPT-3 limits
+    # 500 words input limit
+    # 4,000 characters input limit
+    
+    # GPT-4 limits
+    # 25,000 words limit
+    # ??? characters input limit
+    
+    processor = FolderStructureProcessor(depth_limit=7, line_limit=None, word_limit=250000)
+    processor.get_structure(directory_to_scan, prefix='│   ', whitelisted_filetypes=[".cs"], depth=1, lines=0)
 
-    processor.summarize()
+    # processor.summarize()
